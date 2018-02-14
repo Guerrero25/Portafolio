51455e3f04005eacebf8a1a1dd12a430a7db3516
Omar Guerrero <guerrero9725@gmail.com>
2018-02-13 19:11:52 -0500

    fix: Eliminated navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 
 import './sass/main.css';
-import './sass/navbar.css'
 
-import Navbar from './components/navbar';
 import Main from './components/main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Navbar />
        <Main />
       </div>
     );
